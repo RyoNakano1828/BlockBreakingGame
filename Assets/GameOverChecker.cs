c0f74d7b2caf83126a1b5cba475f9f0ab7d88cd3
ryo_nakano <naca.11868@gmail.com>
2021-01-04 16:51:53 +0900

    Block Breaking Game

---
+++ Assets/GameOverChecker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverChecker : MonoBehaviour
+{
+    public GameObject ball;
+
+    public GameObject panelOnGameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "Ball")
+        {
+            OnGameOver();
+        }
+
+    }
+
+    public void OnGameOver()
+    {
+        panelOnGameOver.SetActive(true);
+        ball.SetActive(false);
+    }
+}
---
+++ Assets/StageState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageState : MonoBehaviour
+{
+    public GameObject allBlocks;
+
+    public GameObject ball;
+
+    public GameObject player;
+
+    public GameObject panelOnStageClear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isClear = true;
+        foreach(Transform Blocks in allBlocks.transform)
+        {
+            if(Blocks.childCount != 0)
+            {
+                isClear = false;
+            }
+        }
+        if(isClear == true)
+        {
+            OnStageClear();
+        }
+        
+    }
+
+    public void OnStageClear()
+    {
+        panelOnStageClear.SetActive(true);
+        ball.SetActive(false);
+    }
+}
